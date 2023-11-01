56b9ecf3475190d7a07c05982217a268811d527e
AymericB305 <aymeric.breton@neos-sdi.com>
2023-11-01 22:09:43 +0100

    feat: tweet data

--- components/timeline/Tweet.vue
+++ components/timeline/Tweet.vue
@@ -1,87 +1,49 @@
 <template>
-  <div class="flex flex-shrink-0 p-4 pb-0">
-    <a href="#" class="flex-shrink-0 block group">
-      <div class="flex items-center">
-        <div>
-          <img class="inline-block w-10 h-10 rounded-full"
-            src="https://pbs.twimg.com/profile_images/1121328878142853120/e-rpjoJi_bigger.png" alt="" />
-        </div>
-        <div class="ml-3">
-          <p class="text-base font-medium leading-6 text-white">
-            Sonali Hirave
-            <span
-              class="text-sm font-medium leading-5 text-gray-400 transition duration-150 ease-in-out group-hover:text-gray-300">
-              @ShonaDesign . 16 April
-            </span>
-          </p>
-        </div>
-      </div>
-    </a>
-  </div>
-  <div class="pl-16">
-    <p class="flex-shrink text-base font-medium text-white width-auto">
-      Day 07 of the challenge <span class="text-blue-400">#100DaysOfCode</span>
-      I was wondering what I can do with <span class="text-blue-400">#tailwindcss</span>, so just started building
-      Twitter UI using Tailwind and so far it looks so promising. I will post my code after completion.
-      [07/100]
-      <span class="text-blue-400"> #WomenWhoCode #CodeNewbie</span>
-    </p>
+  <div class="flex pt-4 pr-4">
+    <div class="pt-2 pl-4 mr-3">
+      <img class="rounded-full w-28"
+        src="https://pbs.twimg.com/profile_images/1121328878142853120/e-rpjoJi_bigger.png" alt="" />
+    </div>
 
-    <div class="flex items-center w-full">
-      <div class="flex-1 text-center">
-        <a href="#"
-          class="flex items-center w-12 px-3 py-2 mt-1 text-base font-medium leading-6 text-gray-500 rounded-full group hover:bg-slate-800 hover:text-blue-300">
-          <svg class="w-6 h-6 text-center" fill="none" stroke-linecap="round" stroke-linejoin="round"
-            stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
-            <path
-              d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z">
-            </path>
-          </svg>
-        </a>
+    <div>
+      <div class="flex items-center gap-2">
+        <p>{{ tweet?.user?.name }}</p>
+        <p class="text-sm text-gray-400">
+          @{{ tweet?.user?.tn }}
+        </p>
       </div>
 
-      <div class="flex-1 py-2 m-2 text-center">
-        <a href="#"
-          class="flex items-center w-12 px-3 py-2 mt-1 text-base font-medium leading-6 text-gray-500 rounded-full group hover:bg-slate-800 hover:text-blue-300">
-          <svg class="w-6 text-center h-7" fill="none" stroke-linecap="round" stroke-linejoin="round"
-            stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
-            <path d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4"></path>
-          </svg>
-        </a>
-      </div>
+      <p class="flex-shrink text-base font-medium text-white width-auto">
+        {{ tweet?.text }}
+      </p>
 
-      <div class="flex-1 py-2 m-2 text-center">
-        <a href="#"
-          class="flex items-center w-12 px-3 py-2 mt-1 text-base font-medium leading-6 text-gray-500 rounded-full group hover:bg-slate-800 hover:text-blue-300">
-          <svg class="w-6 text-center h-7" fill="none" stroke-linecap="round" stroke-linejoin="round"
-            stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
-            <path
-              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z">
-            </path>
-          </svg>
-        </a>
-      </div>
-
-      <div class="flex-1 py-2 m-2 text-center">
-        <a href="#"
-          class="flex items-center w-12 px-3 py-2 mt-1 text-base font-medium leading-6 text-gray-500 rounded-full group hover:bg-slate-800 hover:text-blue-300">
-          <svg class="w-6 text-center h-7" fill="none" stroke-linecap="round" stroke-linejoin="round"
-            stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
-            <path d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"></path>
-          </svg>
-        </a>
-      </div>
-      
-      <div class="flex-1 py-2 m-2 text-center">
-        <a href="#"
-          class="flex items-center w-12 px-3 py-2 mt-1 text-base font-medium leading-6 text-gray-500 rounded-full group hover:bg-slate-800 hover:text-blue-300">
-          <svg class="w-6 text-center h-7" fill="none" stroke-linecap="round" stroke-linejoin="round"
-            stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
-            <path
-              d="M8 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-2m-4-1v8m0 0l3-3m-3 3L9 8m-5 5h2.586a1 1 0 01.707.293l2.414 2.414a1 1 0 00.707.293h3.172a1 1 0 00.707-.293l2.414-2.414a1 1 0 01.707-.293H20">
-            </path>
-          </svg>
-        </a>
+      <div class="flex justify-between w-full">
+        <UButton 
+          icon="i-heroicons-chat-bubble-oval-left"
+          color="black"
+          variant="ghost"
+          :ui="{ padding: 'p-0'}"
+        >{{tweet?.replies?.length}}</UButton>
+        <UButton 
+          icon="i-heroicons-arrow-path-rounded-square"
+          color="black"
+          variant="ghost"
+        >{{tweet?.retweets}}</UButton>
+        <UButton 
+          icon="i-heroicons-heart"
+          color="black"
+          variant="ghost"
+        >{{tweet?.likes}}</UButton>
+        <UButton 
+          icon="i-heroicons-arrow-up-tray"
+          color="black"
+          variant="ghost"
+        />
+        <UButton 
+          icon="i-heroicons-bookmark"
+          color="black"
+          variant="ghost"
+        />
       </div>
     </div>
   </div>
@@ -89,5 +51,8 @@
 </template>
 
 <script lang="ts" setup>
+import type { Tweet } from '~/models/tweet';
+
+defineProps<{ tweet?: Tweet }>()
 
 </script>
