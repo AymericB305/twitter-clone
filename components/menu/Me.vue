ce5bdb0907689a4f50b15296f18009b26722a18a
abreton <abreton@NEOS-NBK31088>
2023-11-07 09:47:08 +0100

    feat: user can upload avatar

--- components/menu/Me.vue
+++ components/menu/Me.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center p-4 mt-12 mr-2">
-    <img class="h-12 mr-2 rounded-full"
-      :src="me?.avatar_path" alt="" />
+    <img class="w-12 h-12 mr-2 rounded-full"
+      :src="data!.signedUrl" alt="" />
     <div class="flex flex-col">
       <p>
         {{ me?.name }}
@@ -16,5 +16,9 @@
 <script lang="ts" setup>
 import type { User } from '~/models/user';
 
-defineProps<{ me?: User }>()
+const props = defineProps<{ me?: User }>()
+
+const supabase = useClient();
+const { data } = await supabase.storage.from('avatars').createSignedUrl(props.me!.email, 60)
+
 </script>
