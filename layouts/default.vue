29a22c0e3edd6c85aa05f4eaf58d8b9b669ecf89
Aymeric Breton <aymericbreton8535@gmail.com>
2024-05-26 23:20:01 +0200

    better layout size

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="flex justify-center min-h-screen gap-2 text-white bg-slate-900">
     <Menu />
     
-    <div class="h-auto border border-t-0 border-gray-600 w-3xl">
+    <div class="w-4/12 h-auto border border-t-0 border-gray-600">
       <h2 class="m-4 text-xl font-semibold text-white capitalize">{{layoutCustomProps.title}}</h2>
       <hr class="border-gray-600">
       <slot />
